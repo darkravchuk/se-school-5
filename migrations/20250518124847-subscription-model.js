4757fac41f509fe6731f1e5a0da7e11fb8bd85f0
Daria Kravchuk <daria1kravchuk@gmail.com>
2025-05-18 16:50:47 +0300

    Create initial migration setup

---
+++ migrations/20250518124847-subscription-model.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Subscriptions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      frequency: {
+        type: Sequelize.ENUM('hourly', 'daily'),
+        allowNull: false
+      },
+      confirmed: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      confirmationToken: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      unsubscribeToken: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Subscriptions');
+  }
+};
